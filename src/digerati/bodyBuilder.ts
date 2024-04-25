a65710a8f7162c2681e4fcbd4758a3488b783933
Digerati <cabal@digerati.design>
2024-04-25 16:55:34 +0100

    Update speech bubble CTA

--- src/digerati/bodyBuilder.ts
+++ src/digerati/bodyBuilder.ts
@@ -6,27 +6,37 @@
 export const bodyBuilder = () => {
     const bodyBuilder = document.querySelector('.home_bodybuilder_animation-wrapper');
     if (bodyBuilder) {
+        const speechBubbleCtas = [
+            '.home_hero_english-cta',
+            '.home_hero_japanese-cta',
+            '.home_hero_german-cta',
+            '.home_hero_spanish-cta'
+        ];
         const bodyBuilderImages = [
             '.home_bodybuilder_english-animation', /* <-- Default */
             '.home_bodybuilder_japanese-animation',
             '.home_bodybuilder_german-animation',
             '.home_bodybuilder_spanish-animation'
         ];
-        let bodyBuilderImageIndex = 0;
+        let languageIndex = 0;
         bodyBuilder.addEventListener('mouseenter', () => {
-            bodyBuilderImageIndex++;
-            if (bodyBuilderImageIndex === bodyBuilderImages.length) {
-                bodyBuilderImageIndex = 1;
+            languageIndex++;
+            if (languageIndex === bodyBuilderImages.length) {
+                languageIndex = 1;
             }
             bodyBuilderImages.forEach(function (item, index) {
                 const bodyBuilderImage = document.querySelector(bodyBuilderImages[index]);
-                bodyBuilderImage.style.opacity = index === bodyBuilderImageIndex ? 100 : 0;
+                const speechBubbleCtaImage = document.querySelector(speechBubbleCtas[index]);
+                bodyBuilderImage.style.opacity = index === languageIndex ? 100 : 0;
+                speechBubbleCtaImage.style.display = index === languageIndex ? 'flex' : 'none';
             });
         });
         bodyBuilder.addEventListener('mouseleave', () => {
             bodyBuilderImages.forEach(function (item, index) {
                 const bodyBuilderImage = document.querySelector(bodyBuilderImages[index]);
+                const speechBubbleCtaImage = document.querySelector(speechBubbleCtaImages[index]);
                 bodyBuilderImage.style.opacity = index === 0 ? 100 : 0;
+                speechBubbleCtaImage.syle.display = index === 0 ? 'flex' : 'none';
             });
         });
     }
